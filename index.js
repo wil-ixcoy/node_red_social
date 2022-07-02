3ad75e2f8f65e96c2315a438e626792fdd03a9cb
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-07-02 14:21:28 -0600

    se crea componente de usuario y el archivo de red para obtener todas las peticiones dentro del componente, este se usa en index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
-const { config } = require('../config');
+const user = require('./components/user/user.network');
+const { config } = require('./config');
 
 app.use(express.json());
 
-app.use('/', (req, res) => {
-    res.send('Hello World!');
-}
-);
+app.use('/api/users', user);
+
 app.listen(config.port, () => {
     console.log('Server is running on port ' + config.port);
 }
